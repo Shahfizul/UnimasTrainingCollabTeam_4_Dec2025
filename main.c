a7e629c5146ea8c0e7efb129aeab3330e83ea6b6
Shahfizul <shahfizul321@gmail.com>
2025-12-06 16:54:51 +0800

    Member 5 Code

--- main.c
+++ main.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+int main(int argc, char *argvI[]) {
+    int a = 13;
+    int b = 10;
+    printf("Simple Calculator\n");
+    printf("a + b = %d\n", add_funct(a,b));
+    printf("a - b = %d\n", sub_funct(a,b));
+    printf("a * b = %d\n", mul_funct(a,b));
+    printf("a / b=%dn", div_funct(a,b));
+    return 0;
+}
